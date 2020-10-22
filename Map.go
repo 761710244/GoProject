ed7b6ce2009fb00b4fc808c417dd0b753c61937b
pinganzhang <1596454155@qq.com>
2020-10-22 16:39:24 +0800

    HashMap

---
+++ Map.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var hashMap map[string]string
+	hashMap = make(map[string]string)
+	hashMap["aa"] = "bb"
+	hashMap["bb"] = "cc"
+
+	for key, value := range hashMap {
+		fmt.Println(key, value)
+	}
+
+	key, exist := hashMap["aa"]
+	if exist {
+		fmt.Println(key, hashMap[key])
+	} else {
+		fmt.Println("No")
+	}
+
+	//initMap
+	hashMap1 := map[int]int{1: 2, 2: 3, 3: 5}
+	for key, value := range hashMap1 {
+		fmt.Println(key, value)
+	}
+	delete(hashMap1, 2)
+	for key, value := range hashMap1 {
+		fmt.Println(key, value)
+	}
+}
